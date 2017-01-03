0a1a361549136ae2217e6a8168549d93ae951d79
Zach Leigh <zachleigh@fastmail.jp>
2017-01-03 18:29:01 +0900

    Issue #34. Set mobile menu z-index to 100.

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ require('laravel-elixir-vue-2');
 
 elixir((mix) => {
     mix.sass('./src/resources/assets/sass/larafolio.scss', './dist/css/')
+    .browserSync({open: false, proxy: 'http://localhost:8000/', notify: false})
         .styles([
             './src/resources/assets/vendor/normalize.css',
             '/../../../node_modules/dropzone/dist/min/dropzone.min.css',
             './dist/css/larafolio.css'
         ], './dist/css/larafolio-final.css')
         .webpack('./src/resources/assets/js/larafolio.js', './dist/js/')
-        .browserSync({open: false, proxy: 'http://localhost:8000/', notify: false})
+        
         .version([
             './dist/css/larafolio-final.css',
             './dist/js/larafolio.js'
