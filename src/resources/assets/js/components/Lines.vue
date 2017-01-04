8aae51d1b6e2368535ebbccdcd34605e310922fe
Zach Leigh <zachleigh@fastmail.jp>
2017-01-04 18:17:27 +0900

    Minifiy lines graphics.

--- src/resources/assets/js/components/Lines.vue
+++ src/resources/assets/js/components/Lines.vue
@@ -1,48 +1,6 @@
 <template>
     <div class="lines">
-        <svg
-           xmlns:dc="http://purl.org/dc/elements/1.1/"
-           xmlns:cc="http://creativecommons.org/ns#"
-           xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-           xmlns:svg="http://www.w3.org/2000/svg"
-           xmlns="http://www.w3.org/2000/svg"
-           xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-           xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-           width="100"
-           height="55"
-           viewBox="0 0 99.999994 55.000001"
-           id="svg2"
-          >
-          <defs
-             id="defs4" />
-          <g
-             inkscape:label="Layer 1"
-             inkscape:groupmode="layer"
-             id="layer1"
-             transform="translate(0,-997.36218)">
-            <rect
-               style="opacity:1;fill-opacity:1;stroke:none;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-               id="rect4140"
-               width="100"
-               height="5"
-               x="-1.9073486e-06"
-               y="1047.3622" />
-            <rect
-               y="997.36218"
-               x="75"
-               height="5"
-               width="25"
-               id="rect4142"
-               style="opacity:1;fill-opacity:1;stroke:none;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
-            <rect
-               style="opacity:1;fill-opacity:1;stroke:none;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-               id="rect4144"
-               width="50"
-               height="5"
-               x="50"
-               y="1022.3622" />
-          </g>
-        </svg>
+        <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" width="100" height="55" viewBox="0 0 100 55" version="1.1"><g transform="translate(0,-997.36218)"><rect width="100" height="5" x="0" y="1047.36"/><rect y="997.36" x="75" height="5" width="25"/><rect width="50" height="5" x="50" y="1022.36"/></g></svg>
     </div>
 </template>
 
